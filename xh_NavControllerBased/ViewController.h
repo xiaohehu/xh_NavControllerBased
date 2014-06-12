26800ef4a5b87100176344712ba15ff8001a3201
xiaohehu <xiaohe.hu@macminiveloper1.neoscape.local>
2014-06-12 11:32:47 -0400

    Initial Commit

---
+++ xh_NavControllerBased/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  xh_NavControllerBased
+//
+//  Created by Xiaohe Hu on 6/12/14.
+//  Copyright (c) 2014 Neoscape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ xh_NavControllerBasedTests/xh_NavControllerBasedTests.m
@@ -0,0 +1,34 @@
+//
+//  xh_NavControllerBasedTests.m
+//  xh_NavControllerBasedTests
+//
+//  Created by Xiaohe Hu on 6/12/14.
+//  Copyright (c) 2014 Neoscape. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface xh_NavControllerBasedTests : XCTestCase
+
+@end
+
+@implementation xh_NavControllerBasedTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
